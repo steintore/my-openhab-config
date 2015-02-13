867b5bf23df2f1ec29752ad630697eb9f9fe2ed8
Stein Tore Tøsse <stein.tore@kodemaker.no>
2015-02-13 16:53:57 +0100

    Try to fix the dimmer in the bathroom and the group of light bulbs

--- transform/getBathroomDimmerSwitch.js
+++ transform/getBathroomDimmerSwitch.js
@@ -3,4 +3,4 @@ JSON.parse(input).data.devices.forEach(function(element, index, array) {
     if (element.id == 'ZWayVDev_zway_14-0-38') {
         returnValue = element.metrics.level;
     }});
-returnValue;
+returnValue + "";
